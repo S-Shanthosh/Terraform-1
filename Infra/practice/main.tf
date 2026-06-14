379237eb4f7d76dcc365d969f3592cf4441459ea
S-Shanthosh <137028121+S-Shanthosh@users.noreply.github.com>
2026-06-14 20:32:23 +0530

    s3-backend added

--- Infra/practice/main.tf
+++ Infra/practice/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "shanthosh-terraform-state-management"
+    key            = "practice/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
 provider "aws" {
 region = "ap-south-1"
 }
