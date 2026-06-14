034f86e9040617534828522bfa642b9a6eaf37e5
S-Shanthosh <137028121+S-Shanthosh@users.noreply.github.com>
2026-06-14 20:05:43 +0530

    ec2-vpc-sg-keypair

--- Infra/practice/main.tf
+++ Infra/practice/main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "practice-ec2" {
 instance_type = "t2.micro"
 ami = "ami-0f58b397bc5c1f2e8"
 key_name = aws_key_pair.shanthosh_key.key_name
+vpc_security_group_ids = [aws_security_group.practice-sg.id]
 
 tags = {
     Name= "practice-ec2"
@@ -26,4 +27,18 @@ tags = {
 resource "aws_key_pair" "shanthosh_key" {
     key_name = "shanthosh-key"
     public_key = file("~/.ssh/shanthosh-key.pub")
+}
+
+
+resource "aws_security_group" "practice-sg" {
+    name = "practice-sg"
+    description = "Allow SSH"
+
+    ingress {
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
 }
