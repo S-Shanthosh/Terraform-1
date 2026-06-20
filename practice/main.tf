ba91b9f9d7985e5b37ffba3b931fc83ffc78bb18
S-Shanthosh <137028121+S-Shanthosh@users.noreply.github.com>
2026-06-20 08:53:36 +0530

    bucket name change in backend config

--- practice/main.tf
+++ practice/main.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "shanthosh-terraform-state-management"
+    key            = "practice/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "shanthosh-terraform-state-management-locks"
+    encrypt        = true
+  }
+}
 
 provider "aws" {
   region = "ap-south-1"
